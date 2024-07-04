e78d747e212d426c4a9b4643bdd338aee022f98e
ThanhTranDz <tranthanh181103@gmail.com>
2024-07-04 09:24:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+function fetchDogImage() {
+    const cardImages = document.querySelectorAll(".slide img")
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+    .then((response) => response.json())
+    .then((data) => {
+      const images = data.message;
+      
+      images.forEach((image, index) => {
+        cardImages[index].src = image;
+      });
+    })
+    .catch((error) => console.error("Error fetching images:", error));
+}
+
+fetchDogImage();
+
+var bgImage = document.getElementsByClassName("current-image")[0].src;
+document.getElementsByClassName("bg")[0].src = bgImage
+
+const buttons = {
+  prev: document.querySelector(".icon_left"),
+  next: document.querySelector(".icon_right"),
+};
+
+buttons.next.addEventListener('click', () => reIndex("icon_right"));
+buttons.prev.addEventListener('click', () => reIndex("icon_left"));
+
+function reIndex(dir){
+  const indexSlide = document.querySelector(".current-slide");
+  const nextSlide = document.querySelector(".next-slide");
+  const prevSlide = document.querySelector(".previous-slide");
+
+  const indexImage = document.querySelector(".current-image");
+  const nextImage = document.querySelector(".next-image");
+  const prevImage = document.querySelector(".previous-image");
+  
+  //teporary disable button after click
+  buttons.next.classList.add("tempo-disable");
+  buttons.prev.classList.add("tempo-disable");
+
+  if(dir === "icon_right"){
+    //switch card index
+    indexSlide.classList.remove("current-slide");
+    indexSlide.classList.add("previous-slide");
+
+    nextSlide.classList.remove("next-slide");
+    nextSlide.classList.add("current-slide");
+
+    prevSlide.classList.remove("previous-slide");
+    prevSlide.classList.add("next-slide");
+
+    //switch image index
+    indexImage.classList.remove("current-image");
+    indexImage.classList.add("previous-image");
+
+    nextImage.classList.remove("next-image");
+    nextImage.classList.add("current-image");
+
+    prevImage.classList.remove("previous-image");
+    prevImage.classList.add("next-image");
+  }else{
+    //switch card index
+    indexSlide.classList.remove("current-slide");
+    indexSlide.classList.add("next-slide");
+
+    nextSlide.classList.remove("next-slide");
+    nextSlide.classList.add("previous-slide");
+
+    prevSlide.classList.remove("previous-slide");
+    prevSlide.classList.add("current-slide");
+
+    //switch image index
+    indexImage.classList.remove("current-image");
+    indexImage.classList.add("next-image");
+
+    nextImage.classList.remove("next-image");
+    nextImage.classList.add("previous-image");
+
+    prevImage.classList.remove("previous-image");
+    prevImage.classList.add("current-image");
+  }
+
+  //set time for temporary disable click
+  setTimeout(()=>{
+    buttons.next.classList.remove("tempo-disable");
+    buttons.prev.classList.remove("tempo-disable");
+  }, 500)
+
+}
